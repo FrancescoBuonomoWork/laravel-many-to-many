105aeea6c4a08cb86e37e698cdd000b7572c38fa
FrancescoBuonomo <buonomofrancescow@gmail.com>
2024-01-10 14:47:31 +0100

    fix model

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Type;
+use App\Models\Technology;
 
 class ProjectSeeder extends Seeder
 {
@@ -16,13 +17,17 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker): void
     {
         $types = Type::all();
-        $ids = $types->pluck("id");
+        $type_id = $types->pluck("id");
+
+        $technologies = Technology::all();
+        $technology_id = $technologies->pluck("id");
 
         for($i = 0; $i < 10; $i++){
             $new_project = new Project();
             $new_project->name = $faker->name();
-            $new_project->type_id = $faker->optional()->randomElement($ids);
+            $new_project->type_id = $faker->optional()->randomElement($type_id);
             $new_project->save();
+            $new_project->technologies()->attach($faker->randomElements($technology_id,null));   
         }
     }
 }
